cd400fa1d4d952e9d153b24d74325d36238d764c
el_jeffe <jeff@nwpodiatric.com>
2022-08-16 16:38:51 -0700

    pos

--- km_hospital/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import patient
-from . import doctor
-from . import department
-from . import appointment
-from . import medicaltest
-from . import equipmentSale
-from . import saleOrder
