e3544f214afd278fe7786e1d419af0343fee40d2
el_jeffe <jeff@nwpodiatric.com>
2022-09-16 16:35:12 -0700

    working on logic

--- pod_hms/model/podiatry_pathology_category.py
+++ pod_hms/model/podiatry_pathology_category.py
@@ -7,7 +7,7 @@ from odoo import models, fields, api, _
 class podiatry_pathology_category(models.Model):
     _name = 'podiatry.pathology.category'
     _description = 'podiatry pathology category'
-
+    # name = fields.Char(string="Category Name")
     name = fields.Char(string="Category Name", required=True)
     active = fields.Boolean(string="Active", default=True)
     parent_id = fields.Many2one(
