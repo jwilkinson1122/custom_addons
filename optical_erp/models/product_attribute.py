2b2964ab7a5ec435308ac5eef16365c37e352a59
el_jeffe <jeff@nwpodiatric.com>
2022-12-09 12:17:39 -0800

    idk

--- optical_erp/models/product_attribute.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-
-class ProductAttribute(models.Model):
-    _inherit = 'product.attribute'
-
-    Sequence = fields.Integer(string="Sequence")
-    in_pos = fields.Boolean(string="Display in POS")
