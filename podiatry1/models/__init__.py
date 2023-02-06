1fa5933365e330df6a1fc9576017c875f4a1350a
el_jeffe <jeff@nwpodiatric.com>
2023-02-06 13:10:00 -0800

    kjgk

---
+++ podiatry1/models/__init__.py
@@ -0,0 +1,8 @@
+# See LICENSE file for full copyright and licensing details.
+
+from . import account_move
+from . import podiatry_prescription
+from . import podiatry_device
+from . import podiatry_services
+from . import product_product
+from . import res_company
---
+++ podiatry1/wizard/sale_make_invoice_advance.py
@@ -0,0 +1,42 @@
+# See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class SaleAdvancePaymentInv(models.TransientModel):
+    _inherit = "sale.advance.payment.inv"
+
+    advance_payment_method = fields.Selection(
+        [
+            ("delivered", "Regular invoice"),
+            ("percentage", "Down payment (percentage)"),
+            ("fixed", "Down payment (fixed amount)"),
+        ],
+        string="Create Invoice",
+        default="delivered",
+        required=True,
+        help="""A standard invoice is issued with all the order lines ready for
+        invoicing, according to their invoicing policy
+        (based on ordered or delivered quantity).""",
+    )
+
+    def create_invoices(self):
+        ctx = self.env.context.copy()
+        if self._context.get("active_model") == "podiatry.prescription":
+
+            PodiatryPrescription = self.env["podiatry.prescription"]
+            prescription = PodiatryPrescription.browse(
+                self._context.get("active_ids", []))
+            prescription.device_line_ids.mapped(
+                "product_id").write({"is_device": True})
+            ctx.update(
+                {
+                    "active_ids": prescription.order_id.ids,
+                    "active_id": prescription.order_id.id,
+                    "prescription_id": prescription.id,
+                }
+            )
+        res = super(SaleAdvancePaymentInv,
+                    self.with_context(**ctx)).create_invoices()
+
+        return res
