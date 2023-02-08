8f4602fcfac5bf421ef1ab0676a115620f335f28
el_jeffe <jeff@nwpodiatric.com>
2023-02-08 13:45:39 -0800

    dsfv

--- podiatry/tests/__init__.py
+++ podiatry/tests/__init__.py
@@ -1,12 +1,17 @@
-from . import test_product_configurator_test_cases
 
 # from . import test_create
 # from . import test_configuration_rules
-from . import test_product
-
 # from . import test_product_attribute
 # from . import test_product_config
 # from . import test_wizard
 # from . import test_sale
 
-from . import test_partner_contact_in_several_companies
+
+from . import (
+    test_partner_contact_in_several_companies,
+    test_product_configurator_test_cases,
+    test_product,
+    test_dimensions,
+    test_product_template,
+    test_weight_in_uom,
+)
