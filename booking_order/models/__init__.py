44c9a02a8d7bd9ff1a83563cd491f08c84479b92
el_jeffe <jeff@nwpodiatric.com>
2023-02-28 10:15:47 -0800

    segf

---
+++ booking_order/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import service_team
+from . import booking_order
+from . import work_order
---
+++ pet_clinic/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import client
+from . import pet
+from . import appointment
+from . import doctor
+from . import item
+from . import service
+from . import visitation
