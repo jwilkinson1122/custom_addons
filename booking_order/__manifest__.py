e821df250b861454887685a5becddef062ee1022
el_jeffe <jeff@nwpodiatric.com>
2023-04-11 11:16:40 -0700

    sf

--- booking_order/__manifest__.py
+++ booking_order/__manifest__.py
@@ -3,18 +3,14 @@
     'name': "Booking Order",
 
     'summary': """
-        Booking Order Zhafron""",
+        Booking Order NWPL""",
 
     'description': """
-        Booking Order Zhafron
+        Booking Order NWPL
     """,
 
-    'author': "Zhafron",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'author': "NWPL",
+    'website': "http://www.nwpodiatric.com",
     'category': 'Sales',
     'version': '0.1',
     'application': True,
