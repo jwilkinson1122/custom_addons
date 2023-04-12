2701142d5c625600697ab12604b7d08bf78818f1
el_jeffe <jeff@nwpodiatric.com>
2023-04-12 10:57:00 -0700

    asd

--- product_variant_configurator/__manifest__.py
+++ product_variant_configurator/__manifest__.py
@@ -1,13 +1,8 @@
-# Copyright 2015 Oihane Crucelaegui (AvanzOSC)
-# Copyright 2016 ACSONE SA/NV
-# Copyright 2017 David Vidal <david.vidal@tecnativa.com>
-# Copyright 2015-2017 Tecnativa - Pedro M. Baeza
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Product Variant Configurator",
     "summary": "Provides an abstract model for product variant configuration.",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Product Variant",
     "license": "AGPL-3",
     "author": "AvanzOSC, Tecnativa, ACSONE SA/NV, Odoo Community Association (OCA)",
