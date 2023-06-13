3d2e0838462383eb63520ba28c9d47927d378d0f
Jeff <jeff@nwpodiatric.com>
2023-06-12 22:19:21 -0700

    awesome tshirts test

--- hospital_dental_management_app/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import dental_questionnaire
--- hospital_management_app/models/product_template_inherited.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, _
-
-
-class ProductTemplateInherit(models.Model):
-    _inherit = "product.template"
-
-    is_food = fields.Boolean(string='Is Food')
