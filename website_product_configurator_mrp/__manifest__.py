512c5d8e80e8bebbeb8646ce36d88124acc44bfe
el_jeffe <jeff@nwpodiatric.com>
2023-08-08 10:48:01 -0700

    asc

---
+++ website_product_configurator_mrp/__manifest__.py
@@ -0,0 +1,30 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Configurator Manufacturing",
+    "version": "14.0.1.0.0",
+    "category": "Website",
+    "summary": "Website integration of MRP",
+    "author": "Pledra, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/product-configurator",
+    "depends": ["product_configurator_mrp", "website_product_configurator"],
+    "data": [
+        "views/templates.xml", 
+        # "views/assets.xml",
+        ],
+    "assets": {
+        "web.assets_frontend": [
+            "website_product_configurator_mrp/static/src/js/website_sale.js",
+        ],
+    },
+    "application": True,
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["PCatinean"],
+    "auto_install": False,
+}
+
+
+    # <xpath expr="//script[last()]" position="after">
+    #         <script type="text/javascript" src="/website_product_configurator_mrp/static/src/js/website_sale.js" />
+    #     </xpath>
